25b706477f226ada8e1d2a77f99ee42785c71c2b
ariblekk <30851927+ariblekk@users.noreply.github.com>
2025-04-02 04:38:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Nginx</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.png" />
+    <link href="./css/style.css" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=Rubik:wght@400;500&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+  
+    <div id="container">
+      <h1 data-value="I ❤️ <?php include "./service/urlparam.php" ?> ">I ❤️ <?php include "./service/urlparam.php" ?> </h1>
+      
+    </div>
+  </body>
+  <script src="./js/mascript.js"></script>
+</html>
